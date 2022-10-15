3771a85bd07f7874bdea705b62ee1c111abacb32
diepanhng0711 <diepanhng0711@gmail.com>
2022-10-15 08:42:15 +0700

    Update

---
+++ Lab01/phptest1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML With PHP Embedded</title>
+</head>
+<body>
+    <p style="font-size: 16px; color: blue">Welcome To My Page</p>
+
+    <?php
+        //First PHP program
+        print("<br> Using PHP is not hard <br>");
+        print("<font color =\"blue\"> <br>");
+    ?>
+
+    and you can learn to use it quickly!
+</body>
+</html>
---
+++ Lab01/phptest2.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Generating HTML from PHP</title>
+    </head>
+
+    <body>
+        <!-- PHP Code -->
+        <?php
+            //
+            // Example script to output HTML tags
+            // 
+
+            /* A script that gets information about the PHP version being used. */
+
+            print("Using PHP has <i>some advantage:</i>");
+            print("<ul>
+                        <li>Speed</li>
+                        <li>Speed of use</li>    
+                        <li>Functionality</li>
+                    </ul>");       # Output bullet list
+        ?>
+    </body>
+</html>
